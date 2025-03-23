32ec110f89fd4b30718066f1442012afcd7dbe26
geekskai <geeks.kai@gmail.com>
2025-03-23 14:28:56 +0800

    refactor: update environment variable names in Supabase client
    The environment variable names were changed from NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY to SUPABASE_URL and SUPABASE_ANON_KEY to align with the updated naming convention in the project configuration.

--- src/db/supabase/client.ts
+++ src/db/supabase/client.ts
@@ -1,12 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 
-import { createBrowserClient } from '@supabase/ssr';
+import { createBrowserClient } from "@supabase/ssr";
 
-import { Database } from './types';
+import { Database } from "./types";
 
 export function createClient() {
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!
   );
 }
