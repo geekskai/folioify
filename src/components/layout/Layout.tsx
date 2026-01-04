fa6213fe9adf07d8ffeadf62f052711d02ce1d23
Gan Kai <geeks.kai@gmail.com>
2026-01-04 17:37:48 +0800

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,9 +5,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative min-h-screen flex flex-col">
       <Header />
-      <a href=" " target="_blank" rel="noopener noreferrer"> 
-        <img src="https://img.aitoolshood.com/8f8751eebc084fc5a3006212f97ebd5a.png" alt="aitoolshood.com" style="height: 54px; width: auto;"/>
-      </a>
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
